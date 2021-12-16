f8f47df72a1045bf9f5a2a451b44ca1d80520506
namolaru <leonard.namolaru@yahoo.com>
2021-12-16 14:29:48 +0100

    add InputGate::getValEnBinaire()

--- InputGate.cpp
+++ InputGate.cpp
@@ -17,3 +17,8 @@ InputGate::~InputGate()
 char InputGate::getInputName() const {
 	return inputName;
 }
+
+// RENVOIE LA VALEUR EN BINAIRE 0 SI C'EST FALSE ET 1 SI C'EST TRUE
+int InputGate::getValEnBinaire() const{
+	return (int) val;
+}
