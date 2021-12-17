d41045458764bac7dd95e1a5bb896698a5cafc6a
namolaru <leonard.namolaru@yahoo.com>
2021-12-17 17:34:52 +0100

    Circuit.cpp

--- src/Circuit.cpp
+++ src/Circuit.cpp
@@ -5,42 +5,75 @@ void Circuit::affichageInputs(){
 	for(unsigned int i = 0 ; i < inputs->size() ; i++) {
 		affichageCircuit->push_back( new vector<char> ) ;
 		affichageCircuit->at(i)->push_back( inputs->at(i)->getInputName() );
-		affichageCircuit->at(i)->push_back( ' ' );
 		affichageCircuit->at(i)->push_back( ':' );
-		affichageCircuit->at(i)->push_back( ' ' );
 		affichageCircuit->at(i)->push_back(  to_string( inputs->at(i)->getValEnBinaire() ).at(0) );
+		affichageCircuit->at(i)->push_back( ' ' );
 
-		int counter = 0;
+		unsigned int counter = 0;
 		for(unsigned int j = 0 ; j < gates->size() ; j++){
 			if( gates->at(i)->getEntrees()->at(i) ==  inputs->at(i) )
 				counter++;
 		}
 
-        int nbEtoiles=0;
-		for(unsigned int k = 5 ; nbEtoiles!=counter ; k+=5) {
-			affichageCircuit->at(i)->push_back( '-' );
-			affichageCircuit->at(i)->push_back( '-' );
-			affichageCircuit->at(i)->push_back( '-' );
-			affichageCircuit->at(i)->push_back( '-' );
+		affichageCircuit->at(i)->push_back( '-' );
+		affichageCircuit->at(i)->push_back( '-' );
+
+		for(unsigned int k = 0 ; k < counter ; k++) {
 	
 			if (i == 0){
 				affichageCircuit->at(i)->push_back( '*' );
-				nbEtoiles++;
-			}else{
-				if(k+4 < affichageCircuit->at(i-1)->size()){
-                        if (affichageCircuit->at(i-1)->at(k+4)=='*' || affichageCircuit->at(i-1)->at(k+4)=='+' ){
+				if(k != counter - 1)
+					affichageCircuit->at(i)->push_back( '-' );
+			}else{ // <=> (i != 0)
+				if( k+6 < affichageCircuit->at(i-1)->size() ){
+                        if (affichageCircuit->at(i-1)->at(k+6) == '*' || affichageCircuit->at(i-1)->at(k+6) == '+'){
 						  affichageCircuit->at(i)->push_back( '+' );
+						  affichageCircuit->at(i)->push_back( '-' );
+	                      affichageCircuit->at(i)->push_back( '*' );
+							if(k != counter - 1)
+								affichageCircuit->at(i)->push_back( '-' );
+
 					    }else{
 							  affichageCircuit->at(i)->push_back( '*' );
-							  nbEtoiles++;
 						} // else
 				}else{
                        affichageCircuit->at(i)->push_back( '*' );
-					   nbEtoiles++;
 				} // else
 			} // else
 		} // for(k)
 	} // for(i)
+
+	unsigned int indexMaxSize = 0 , maxSize = affichageCircuit->at(0)->size();
+	for(unsigned int i = 1 ; i < affichageCircuit->size() ; i++) {
+		if( affichageCircuit->at(i)->size() > maxSize) {
+			maxSize = affichageCircuit->at(i)->size();
+			indexMaxSize= i;
+		}
+	}
+
+	for(unsigned int i = 0 ; i < affichageCircuit->size() ; i++) {
+		unsigned int sizeI = affichageCircuit->at(i)->size();
+		if( sizeI < maxSize) {
+			if( i < indexMaxSize){
+				affichageCircuit->at(i)->pop_back();
+				for(unsigned int j = 0; j < (maxSize - sizeI) + 2 ; j++)
+					affichageCircuit->at(i)->push_back( '-' );
+				affichageCircuit->at(i)->push_back( '*' );
+
+				if(i != affichageCircuit->size() - 1) {
+					for(unsigned int j = 0; j < (affichageCircuit->at(i)->size() - affichageCircuit->at(i+1)->size()) ; j++)
+						affichageCircuit->at(i +1)->push_back( ' ' );
+					affichageCircuit->at(i +1)->push_back( '|' );
+				}
+
+			}
+		}
+
+	}
+
+
+
+
 } // affichageInputs()
 
 
