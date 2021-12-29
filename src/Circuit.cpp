8e1fb1151460719d4f1eac20f01e26f654dd4b9b
namolaru <leonard.namolaru@yahoo.com>
2021-12-29 18:25:50 +0100

    Circuit.cpp

--- src/Circuit.cpp
+++ src/Circuit.cpp
@@ -2,76 +2,138 @@
 
 
 void Circuit::affichageInputs(){
-	for(unsigned int i = 0 ; i < inputs->size() ; i++) {
-		affichageCircuit->push_back( new vector<char> ) ;
-		affichageCircuit->at(i)->push_back( inputs->at(i)->getInputName() );
-		affichageCircuit->at(i)->push_back( ':' );
-		affichageCircuit->at(i)->push_back(  to_string( inputs->at(i)->getValEnBinaire() ).at(0) );
-		affichageCircuit->at(i)->push_back( ' ' );
+	// la debut de chqaue ligne pour un input ressemble à ceci (par exemple) : a:0 -- (6 caractères)
+	const int NB_CHAR_AU_DEBUT_CHAQUE_LIGNE = 6; // Le nombre de caractères qui apparaissent toujours au début de chaque ligne d'un input
 
-		unsigned int counter = 0;
-		for(unsigned int g = 0 ; g < gates->size() ; g++){
-			for(unsigned int j = 0 ; j < gates->at(g)->getEntrees()->size() ; j++){
+	for(unsigned int i = 0 ; i < inputs->size() ; i++) { // Nous parcourons le vecteur qui inclut les entrées (inputs)
+
+		/*  Les étapes suivantes seront effectuées pour chacune des entrées du circuit   */
+
+		/*  Étape (1) : La ligne commence par afficher le nom de l'entrée, sa valeur booléenne ainsi que quelques caractères supplémentaires pour l'affichage   */
+
+		affichageCircuit->push_back( new vector<char> ) ; // Pour chaque entrée, nous ajoutons une nouvelle ligne à l'affichage
+
+		affichageCircuit->at(i)->push_back( inputs->at(i)->getInputName() ); // Chaque ligne d'entrée commence par l'affichage du nom de l'entrée
+		affichageCircuit->at(i)->push_back( ':' ); // Après le nom d'entrée, nous affichons le caractère ':'
+
+		// Après le caractère ':', on présente la valeur booléenne initiale
+		// Nous obtenons la valeur booléenne en tant que int, nous transformons l'int en string,
+		// puis en utilisant la méthode at() nous obtenons ce nombre en tant que caractère (char) afin qu'il puisse être ajouté au vecteur d'affichage qui se compose de caractères
+		affichageCircuit->at(i)->push_back(  to_string( inputs->at(i)->getValEnBinaire() ).at(0) ); // to_string(int __val) ; char& string.at(size_type __n)
+
+		affichageCircuit->at(i)->push_back( ' ' ); // Ajout d'un espace vide à l'affichage
+		affichageCircuit->at(i)->push_back( '-' ); // Ajout du caractère '-' à l'affichage
+		affichageCircuit->at(i)->push_back( '-' ); // Ajout du caractère '-' à l'affichage
+
+		// Jusqu'à présent, la ligne ressemble à ceci (par exemple) : a:0 --
+
+		/*  Étape (2)  Nous voulons dessiner une trajectoire de départ pour l'entrée, principalement en utilisant le caractère '_'.
+		 * Dans l'itinéraire, nous ajouterons le caractère '*' en fonction du nombre de portes logiques qui reçoivent l'entrée actuelle en input.
+		 * Le caractère '+' est en fait un point qui permet le passage pour d'autres entrées
+		 */
+
+		/** Combien de portes logiques reçoivent en entrée l'elemet i du vecteur inputs ? **/
+		unsigned int counter = 0; // On stocke dans cette variable le nombre de portes logiques qui reçoivent l'input courant en entrée
+		for(unsigned int g = 0 ; g < gates->size() ; g++){ // Nous parcourons le vecteur des portes logiques
+			for(unsigned int j = 0 ; j < gates->at(g)->getEntrees()->size() ; j++){ // Pour chacune des portes logiques, nous parcourons son attribut de type vecteur qui comprend une liste de toutes les entrées de cette porte logique
 				if( gates->at(g)->getEntrees()->at(j) ==  inputs->at(i) )
 					counter++;
 			}
 		}
 
-		affichageCircuit->at(i)->push_back( '-' );
-		affichageCircuit->at(i)->push_back( '-' );
-
-		for(unsigned int k = 0 ; k < counter ; k++) {
+		// Maintenant, nous voulons ajouter le caractère '*' en fonction du nombre de portes logiques qui reçoivent ce input en entrée
+		for(unsigned int g = 0 ; g < counter ; g++) { // La variable 'counter' : le nombre de portes logiques qui reçoivent l'input courant en entrée
 	
-			if (i == 0){
-				affichageCircuit->at(i)->push_back( '*' );
-				if(k != counter - 1)
-					affichageCircuit->at(i)->push_back( '-' );
-			}else{ // <=> (i != 0)
-				if( k+6 < affichageCircuit->at(i-1)->size() ){
-                        if (affichageCircuit->at(i-1)->at(k+6) == '*' || affichageCircuit->at(i-1)->at(k+6) == '+'){
-						  affichageCircuit->at(i)->push_back( '+' );
-						  affichageCircuit->at(i)->push_back( '-' );
-	                      affichageCircuit->at(i)->push_back( '*' );
-							if(k != counter - 1)
-								affichageCircuit->at(i)->push_back( '-' );
+			if (i == 0){ // Si c'est la première entrée du circuit
+				affichageCircuit->at(i)->push_back( '*' ); // Ajout du caractère '*' à l'affichage
+				if(g != counter - 1) // Si l'on s'attend à ajouter encore de caractères de type '*' (c'est-à-dire : si le nombre de caractères '*' ne correspond pas encore à la valeur stockée dans la variable 'counter'
+					affichageCircuit->at(i)->push_back( '-' ); // Ajout du caractère '-' à l'affichage pour que ce caractère sépare les caractères de type '*'
+			}else{ // <=> (i != 0) // Si ce n'est PAS la première entrée du circuit
+				unsigned int longueurLignePrecedente = affichageCircuit->at(i-1)->size();
+				unsigned int longueurLigneActuelle = affichageCircuit->at(i)->size();
 
-					    }else{
-							  affichageCircuit->at(i)->push_back( '*' );
-						} // else
-				}else{
-                       affichageCircuit->at(i)->push_back( '*' );
-				} // else
+				// Si nous n'avons pas dépassé la longueur de la ligne précédente, nous devons vérifier ce qui se passe dans la ligne précédente
+				// Nous voulons ajouter '*'. Mais si la ligne ci-dessus contient à cette position '*' ou '+' ...
+                while (( longueurLigneActuelle < longueurLignePrecedente ) && (affichageCircuit->at(i-1)->at(longueurLigneActuelle) == '*' || affichageCircuit->at(i-1)->at(longueurLigneActuelle) == '+')){
+                	affichageCircuit->at(i)->push_back( '+' ); // Ajout du caractère '+' à l'affichage (un point qui permet le passage pour d'autres entrées)
+					affichageCircuit->at(i)->push_back( '-' ); // Ajout du caractère '-' à l'affichage pour que ce caractère sépare les caractères de type '*' et de type '+'
+
+					longueurLigneActuelle = affichageCircuit->at(i)->size(); // Nous avons ajouté des éléments dans le vecteur et nous devons donc mettre à jour la valeur stockée dans cette variable
+                }
+
+                // La boucle est utilisée car après avoir ajouté les caractères '+' et '-',
+                // nous devons nous assurer que même dans le nouvel emplacement où nous voulons ajouter le caractère '*',
+                // il n'y a pas au dessus un caractère de type '*' ou '+'
+
+				affichageCircuit->at(i)->push_back( '*' ); // Ajout du caractère '*' à l'affichage
+                if(g != counter - 1) // Si l'on s'attend à ajouter encore de caractères de type '*' (c'est-à-dire : si le nombre de caractères '*' ne correspond pas encore à la valeur stockée dans la variable 'counter'
+						affichageCircuit->at(i)->push_back( '-' ); // Ajout du caractère '-' à l'affichage pour que ce caractère sépare les caractères de type '*'
 			} // else
-		} // for(k)
+		} // for(g)
 	} // for(i)
 
-	unsigned int indexMaxSize = 0 , maxSize = affichageCircuit->at(0)->size();
+
+	/* Étape (3) : Nous voulons que toutes les lignes qui tracent le début du chemin des entrées aient la même longueur.
+	 */
+
+	// Nous recherchons en premier lieu la ligne la plus longue
+	unsigned int maxSize = affichageCircuit->at(0)->size();
 	for(unsigned int i = 1 ; i < affichageCircuit->size() ; i++) {
-		if( affichageCircuit->at(i)->size() > maxSize) {
+		if( affichageCircuit->at(i)->size() > maxSize)
 			maxSize = affichageCircuit->at(i)->size();
-			indexMaxSize= i;
-		}
 	}
 
-	for(unsigned int i = 0 ; i < affichageCircuit->size() ; i++) {
-		unsigned int sizeI = affichageCircuit->at(i)->size();
-		if( sizeI < maxSize) {
-			if( i < indexMaxSize){
-				affichageCircuit->at(i)->pop_back();
-				for(unsigned int j = 0; j < (maxSize - sizeI) + 2 ; j++)
-					affichageCircuit->at(i)->push_back( '-' );
-				affichageCircuit->at(i)->push_back( '*' );
+	// Une fois que nous avons localisé quelle est la longueur de le ligne la plus longue,
+	// nous effectuons un ajustement du reste des lignes en fonction de cette longueur
 
-				if(i != affichageCircuit->size() - 1) {
-					for(unsigned int j = 0; j < (affichageCircuit->at(i)->size() - affichageCircuit->at(i+1)->size()) ; j++)
-						affichageCircuit->at(i +1)->push_back( ' ' );
-					affichageCircuit->at(i +1)->push_back( '|' );
-				}
+	for(unsigned int i = 0 ; i < affichageCircuit->size() ; i++) { // Nous parcourons toutes les lignes de l'affichage du circuit (qui comprend actuellement une ligne pour chaque entrée)
+		unsigned int longueurLigneActuelle = affichageCircuit->at(i)->size(); // La longueur de la ligne actuelle
+
+			if( longueurLigneActuelle < maxSize){ // Si la longueur de ligne actuelle est inférieure à la longueur de ligne maximale
+				char dernierElement = affichageCircuit->at(i)->at(longueurLigneActuelle - 1); // On stocke dans une variable la valeur du dernier caractère de la ligne courante
+				affichageCircuit->at(i)->pop_back(); // Nous supprimons le dernier caractère de la ligne actuelle
+
+				for(unsigned int j = 0; j < (maxSize - longueurLigneActuelle) + 2 ; j++) {
+
+							// S'il s'agit de la première ligne alors nous n'avons pas besoin de prendre en compte ce qui se passe dans les lignes précédentes
+					        // (verifier s'il faut ajouter le caractère '+') et nous ajoutons juste le caractère '-'
+							if(i == 0) { // Si c'est la première entrée du circuit
+								affichageCircuit->at(i)->push_back( '-' );
+							}
+							else {
+								if(affichageCircuit->at(i - 1)->at(NB_CHAR_AU_DEBUT_CHAQUE_LIGNE + j - 1) == '*') // si la ligne ci-dessus contient à cette position '*'
+									affichageCircuit->at(i)->push_back( '+' );
+								else
+									affichageCircuit->at(i)->push_back( '-' );
+							}
+
+							longueurLigneActuelle = affichageCircuit->at(i)->size(); // Nous avons ajouté des éléments dans le vecteur et nous devons donc mettre à jour la valeur stockée dans cette variable
+				} // for(j)
+				affichageCircuit->at(i)->push_back( dernierElement ); // Ajout de l'élément que nous avons supprimé de la fin de la ligne
+			} // if( longueurLigneActuelle < maxSize)
+
+			for(unsigned int j = 0; j < affichageCircuit->at(i)->size() ; j++) {
+				if(i != 0) {
+					if(affichageCircuit->at(i)->at(j) == '+') {
+						if(affichageCircuit->at(i - 1)->at(j) != '+' || affichageCircuit->at(i - 1)->at(j) != '*'){
+							affichageCircuit->at(i)->erase( affichageCircuit->at(i)->begin() + j  );
+							affichageCircuit->at(i)->erase( affichageCircuit->at(i)->begin() + (j - 1)  );
+
+						}
+					}
+				} // if(i >= 2)
 
 			}
-		}
 
-	}
+			if(i != affichageCircuit->size() - 1) {
+				for(unsigned int j = 0; j < (affichageCircuit->at(i)->size() - affichageCircuit->at(i+1)->size()) + 1 ; j++)
+					affichageCircuit->at(i +1)->push_back( ' ' );
+				affichageCircuit->at(i +1)->push_back( '|' );
+			}
+
+
+
+		} // for(i)
 
 } // affichageInputs()
 
