6319319ab6b300fc4fd305e078e19d8621e273b6
namolaru <leonard.namolaru@yahoo.com>
2022-01-16 19:10:04 +0100

    Ajustements et commentaires

--- src/NorGate.cpp
+++ src/NorGate.cpp
@@ -5,11 +5,22 @@
  * @author AIT BENALI faycal, NAMOLARU leonard
  */
 #include "NorGate.hpp"
+#include "CircuitException.hpp"
 
 /**
  * Constructeur.
  */
 NorGate::NorGate(Gate *g1 , Gate *g2) : Gate{"NOR", false} {
+	// La longueur du nom d'une entrée qui est aussi une entrée du circuit est d'un caractère,
+	// la longueur du nom d'une entrée qui est une porte logique est de 3 caractères.
+	try {
+		if(g1->getName().size() != g2->getName().size())
+			throw CircuitException("Une porte logique peut avoir comme entrées deux entrées du circuit ou deux entrées qui sont des portes logiques. Mais il n'est pas possible de transférer une entrée du premier type et une seconde entrée du second type.");
+	} catch(const CircuitException& circuitException) {
+	cerr << circuitException.getMessage() << endl;
+	exit(EXIT_FAILURE);
+	}
+
 	entrees->push_back(g1);
 	entrees->push_back(g2);
 }
